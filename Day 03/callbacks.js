97069c777a4f23b39f9f70fafd98193024544abd
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-07-13 11:42:49 +0530

    code pushed for day 03

---
+++ Day 03/callbacks.js
@@ -0,0 +1,54 @@
+
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// const A =function(){
+//     return 100;
+// }
+
+// const B =function(){
+//     return 200;
+// }
+
+// let rv=sum(A,B)
+// console.log(rv);
+
+// function gun(){
+//     console.log("i am gun")
+//     return "gunnnn";
+// }
+// function fun(gun){
+//     console.log("i am fun");
+//     gun();
+
+//     return "funnn";
+
+// }
+
+// fun();
+
+
+
+// let arr=[10,20,30,40,50]
+// function cb(maal){
+//     console.log(maal);
+//     return maal;
+
+// }
+
+// arr.forEach(cb);
+
+// function test2(){
+//     console.log("i am test 2 ");
+// }
+
+// function testing(test2){
+//     console.log("i am taesting 1");
+//     test2();
+
+// }
+
+// testing(test2);
